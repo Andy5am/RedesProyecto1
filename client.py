2d8fa66a75b598d42059ec72a77f8f27b186fd13
Andy5am <38666859+Andy5am@users.noreply.github.com>
2021-08-12 16:29:59 -0600

    Comments

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import logging
-import base64
 import slixmpp
 from slixmpp.exceptions import IqError, IqTimeout
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
+#logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
 
+#Color
 RED   = "\033[1;31m"  
 BLUE  = "\033[1;34m"
 CYAN  = "\033[1;36m"
@@ -14,42 +14,27 @@ BOLD    = "\033[;1m"
 REVERSE = "\033[;7m"
 ENDC = '\033[0m'
 
+#Class wiht al the XMPP functions
 class Client(slixmpp.ClientXMPP):
     
     def __init__(self, jid, password):
         slixmpp.ClientXMPP.__init__(self, jid, password)
 
+        #Event listeners
         self.add_event_handler("session_start", self.start)
         self.add_event_handler('register', self.register)
-
         self.add_event_handler("message", self.get_message)
         self.add_event_handler("chatstate_composing", self.receive_notification)
 
     async def start(self, event):
+        #Get contacts and setting status after loging in
         self.send_presence()
         await self.get_roster()
 
-        def delete_account():
-            self.register_plugin('xep_0030') # Service Discovery
-            self.register_plugin('xep_0066')
-            self.register_plugin('xep_0199') # xmpp Ping
-            self.register_plugin('xep_0004')
-            self.register_plugin('xep_0077')
-
-            response = self.Iq()
-            response['type'] = 'set'
-            response['from'] = self.boundjid.user
-            response['register']['remove'] = True
-            response.send()
-
-            self.disconnect()
-        
-
-        
-        show = True
+        #Menu after logging in or creating account
         menu = '''
         1. Log Out
-        2. Delte Account
+        2. Delete Account
         3. Show contacts
         4. Contact Details
         5. Add Contact
@@ -59,6 +44,9 @@ class Client(slixmpp.ClientXMPP):
         9. Define Presence
         10. Chat Answers
         '''
+        
+        #Loop for menu options
+        show = True
         while(show):
             print('-'*50)
             print(menu)
@@ -66,14 +54,16 @@ class Client(slixmpp.ClientXMPP):
 
             choose = input('Choose an option: ')
 
+            #Functions for every option
             if choose == '1':
                 print('Log Out')
                 self.disconnect()
                 show = False
             elif choose == '2':
                 print('Delete Account')
-                delete_account()
+                self.delete_account()
                 show = False
+                print(GREEN+'Account Deleted'+ENDC)
             elif choose == '3':
                 print(GREEN+'Show Contacts'+ENDC)
                 self.show_contacts()
@@ -96,59 +86,61 @@ class Client(slixmpp.ClientXMPP):
                 print('Define Presence')
                 self.change_presence()
             elif choose == '10':
-                print('Chat Answers')
+                print(GREEN+'Chat Answers'+ENDC)
             else:
-                print('Invalid Option')
+                print(RED+'Invalid Option'+ENDC)
 
             await self.get_roster()
 
-    def invite_group(self):
-        room = input("Write the group name: ")
-        username = input("Who do you wanna invite: ")
-        self.plugin['xep_0045'].invite(room=room+'@conference.alumchat.xyz', jid=username)
+    #Function to delete an account from the server
+    def delete_account(self):
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin('xep_0066')
+        self.register_plugin('xep_0199') # xmpp Ping
+        self.register_plugin('xep_0004')
+        self.register_plugin('xep_0077')
 
+        response = self.Iq()
+        response['type'] = 'set'
+        response['from'] = self.boundjid.user
+        response['register']['remove'] = True
+        response.send()
+
+        self.disconnect()
+        
+    #Function to join a group chat
     def join_group(self):
-        #self.plugin['xep_0045'].join_muc("Prueba@conference.redes2020.xyz", "pruebaaa")
         room = input("Write the group name: ")
         nickname = input("write your nickname: ")
         self.plugin['xep_0045'].join_muc(room+"@conference.alumchat.xyz", nickname)
-        #self.plugin['xep_0045'].set_affiliation(room="hola@conference.alumchat.xyz",  affiliation='member',jid=self.boundjid.full,)
+    
+    #Function to send a message to a group
     def send_group_message(self):
-        
-        #self.plugin['xep_0045'].join_muc('grupo', 'prueba')
-        #group_name = input('Escriba el nombre del grupo: ')
-        #group_name = 'hola@conference.alumchat.xyz'
         room = input("Write the group name: ")
         message = input('Write your message: ')
-        #message = 'hola'
         self.send_message(mto=room+"@conference.alumchat.xyz", mbody=message, mtype='groupchat')
     
+    #Function to send a private message
     def private_message(self):
-        #recipient = 'andy@alumchat.xyz'
-        #message = 'Hola'
-
-        recipient = input('Who are you sending the message to: ')
-        message = input('Write your message: ')
+        recipient = input('Who are you sending the message to (user@alumchat.xyz): ')
         self.send_notification(recipient, "composing")
+        message = input('Write your message: ')
         try:
             self.send_notification(recipient, "paused")
             self.send_message(mto=recipient, mbody=message, mtype='chat')
-            print('Se envio el mensaje')
-            logging.info('Message sent')
-
+            print(GREEN+'Se envio el mensaje'+ENDC)
         except IqError:
-            logging.error('Error in message')
+            print(RED+'Error in message send'+ENDC)
         except IqTimeout:
-            logging.error('No server response')
+            print(RED+'No server response'+ENDC)
 
+    #Function to show the detail from a specific contact
     def show_contact_details(self):
         self.get_roster()
-
         username = input('Write the username: ')
-        #username = 'andy@alumchat.xyz'
 
         contact = self.client_roster[username]
-        print('-'*40)
+        print('*'*50)
         if contact['name']:
             print(BLUE+'Nombre: '+CYAN, contact['name'],ENDC+'\n')
         print(BLUE+'Username: '+CYAN,username,ENDC+'\n')
@@ -160,13 +152,14 @@ class Client(slixmpp.ClientXMPP):
             for client, status in connections.items():
                 print(BLUE+'Estado: '+CYAN,status['status'],ENDC)
 
-        print('-'*40)
+        print('*'*50)
 
+    #Function to show every contact and group
     def show_contacts(self):
         groups = self.client_roster.groups()
         
         for group in groups:
-            print('-'*25)
+            print('*'*50)
             for username in groups[group]:
                 name = self.client_roster[username]['name']
                 if name:
@@ -179,14 +172,21 @@ class Client(slixmpp.ClientXMPP):
                 for client, status in connections.items():
                     print(BLUE+'Estado: '+CYAN,status['status'],ENDC)
                 print('\n')
-        print('-'*25)
+        print('*'*50)
 
+    #Function to add a contact
     def add_contact(self):
         contact = input("New contact username: ")
-        #contact = 'andy@alumchat.xyz'
-        self.send_presence_subscription(pto=contact)
+        try:
+            self.send_presence_subscription(pto=contact)
+            print(GREEN+'Contact Added'+ENDC)
+        except:
+            print(RED+'Failed to add contact'+ENDC)
 
+    #Function to change the presence
     def change_presence(self):
+
+        #Loop to choose form the different presence options
         loop = True
         while(loop):
             print(
@@ -210,23 +210,32 @@ class Client(slixmpp.ClientXMPP):
                 status = 'Do not Disturb'
                 loop = False
             else:
-                print('Opcion invalida')
+                print(RED+'Opcion invalida'+ENDC)
         try:
             self.send_presence(pshow=presence_show, pstatus=status)
-            logging.info('Presence set')
+            print(GREEN+'Presence set'+ENDC)
         except IqError:
-            logging.error('Error al enviar presencia')
+            print(RED+'Error al enviar presencia'+ENDC)
         except IqTimeout:
-            logging.error('No hubo respuesta del servidor')
+            print(RED+'No hubo respuesta del servidor'+ENDC)
 
-
+    #Function when you receive a message
     def get_message(self, message):
-        sender = str(message['from']).split('/')
-        print('*'*50)
-        print(BLUE+'Mensaje de: '+CYAN,sender[0],ENDC)
-        print(CYAN,message['body'],ENDC)
-        print('*'*50)
-            
+        #When the message received is from a group
+        if (message['type']=='groupchat'):
+            sender = str(message['from']).split('/')
+            print('*'*50)
+            print(BLUE+'Grupo: '+CYAN,sender[0],ENDC)
+            print(BLUE,sender[1],': '+CYAN,message['body'],ENDC)
+            print('*'*50)
+        else: #When the message received from a private conversation
+            sender = str(message['from']).split('/')
+            print('*'*50)
+            print(BLUE+'Mensaje de: '+CYAN,sender[0],ENDC)
+            print(CYAN,message['body'],ENDC)
+            print('*'*50)
+
+    #Function to send notification while typing a message        
     def send_notification(self, recipient, state):
         try:
             notification = self.Message()
@@ -234,55 +243,55 @@ class Client(slixmpp.ClientXMPP):
             notification["to"] = recipient
 
             notification.send()
-            logging.info('Se envio la notificacion')
+            print(GREEN+'Se envio la notificacion'+ENDC)
         except IqError:
-            logging.error('Problema con notificacion')
+            print(RED+'Problema con notificacion'+ENDC)
         except IqTimeout:
-            logging.error('Problema en el servidor')
-        
+            print(RED+'Problema en el servidor'+ENDC)
+    
+    #Function when you receive a notification
     def receive_notification(self, chatstate):
-        # Recibir notificaciones
         sender = str(chatstate['from']).split('/')
         print('*'*50)
         print(BLUE,sender[0],CYAN+ 'is typing',ENDC)
         print('*'*50)
 
-    def sendFile(self):
-        with open('./text.txt') as img:
-            image = base64.b64encode(img.read()).decode('utf-8')
-        self.send_message(mto="andy@alumchat.xyz", mbody=image, msubject='file', mtype='chat')
-
+    #Function to register a new account to the server from the client menu
     async def register(self, iq):
-
         responce = self.Iq()
-
         responce['type']='set'
         responce['register']['username'] = self.boundjid.user
         responce['register']['password'] = self.password
 
         try:
             await responce.send()
-            logging.info("Account created")
+            print(GREEN+"Account created"+ENDC)
         except IqError as e:
-            logging.error("Could not register account")
+            print(RED+"Could not register account"+ENDC)
             self.disconnect()
         except IqTimeout:
-            logging.error("No response from server")
+            print(RED+"No response from server"+ENDC)
             self.disconnect()
 
+#Function to register a new account from the main menu
 def register(user, password):
     client = Client(user, password)
-    client.register_plugin("xep_0030")
-    client.register_plugin("xep_0004")
-    client.register_plugin("xep_0199")
-    client.register_plugin("xep_0066")
-    client.register_plugin("xep_0077")
+    client.register_plugin("xep_0030") # Service Discovery
+    client.register_plugin("xep_0004") # Data Forms
+    client.register_plugin("xep_0199") # XMPP Ping
+    client.register_plugin("xep_0066") # Out-of-band Data
+    client.register_plugin("xep_0077") # In-band Registration
+    client.register_plugin('xep_0071') # XHTML-IM
+    client.register_plugin("xep_0085") # Chat State Notifications
+    client.register_plugin('xep_0128') # Service Discovery Extensions
+    client.register_plugin('xep_0045') # Multi user chat
 
     client['xep_0077'].force_registrarion = True
 
     client.connect()
     client.process()
 
+#Funtion to log in from the main menu
 def login(username, password):
     client = Client(username, password)
     client.register_plugin("xep_0004") # Data forms
@@ -292,7 +301,7 @@ def login(username, password):
     client.register_plugin("xep_0085") # Chat State Notifications
     client.register_plugin('xep_0128') # Service Discovery Extensions
     client.register_plugin("xep_0199") # XMPP Ping
-    client.register_plugin('xep_0045')
+    client.register_plugin('xep_0045') # Multi user chat
 
     client.connect()
     client.process(forever=False)
@@ -319,15 +328,11 @@ while(start):
         print('Log In')
         user = input('Write your username (nombre@alumchat.xyz): ')
         password = input('Write your password: ')
-        # user='andy@alumchat.xyz'
-        # password='12345'
         login(user, password)
     elif option=='2': #Sign Up option
         print('Sign Up')
         username = input('Write your username (nombre@alumchat.xyz): ')
         password = input('Write your password: ')
-        # username='pruebaa@alumchat.xyz'
-        # password='12345'
         register(username, password)
     elif option=='3': #Quit option
         start = False
